964b8b9b747df2abbffbbc82f8d3d374d5215923
pictos <pedrojesus.cefet@gmail.com>
2022-06-06 20:57:19 -0300

    Add project files.

---
+++ MFCC/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MFCC;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ MFCC/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MFCC;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
