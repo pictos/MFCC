490f1698dfb14e72cec9307bd8e7237a2ba623f6
pictos <pedrojesus.cefet@gmail.com>
2022-06-06 21:18:05 -0300

    Created ImageTintColorControl

--- MFCC/AppShell.xaml.cs
+++ MFCC/AppShell.xaml.cs
@@ -1,9 +1,13 @@
-﻿namespace MFCC;
+﻿using MFCC.Pages;
+
+namespace MFCC;
 
 public partial class AppShell : Shell
 {
     public AppShell()
     {
         InitializeComponent();
+
+        Routing.RegisterRoute(nameof(ImageControlPage), typeof(ImageControlPage));
     }
 }
