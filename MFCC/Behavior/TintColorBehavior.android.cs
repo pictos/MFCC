dbbba0f35c5ff76ea99b732e003762bc2bfa2faf
pictos <pedrojesus.cefet@gmail.com>
2022-06-08 12:53:49 -0300

    finished

---
+++ MFCC/Behavior/TintColorBehavior.android.cs
@@ -0,0 +1,17 @@
+﻿using Android.Widget;
+
+namespace MFCC
+{
+    public partial class TintColorBehavior : PlatformBehavior<Image, ImageView>
+    {
+        protected override void OnAttachedTo(Image bindable, ImageView platformView)
+        {
+            ImageEx.ApplyColor(platformView, TintColor);
+        }
+
+        protected override void OnDetachedFrom(Image bindable, ImageView platformView)
+        {
+            ImageEx.ClearColor(platformView);
+        }
+    }
+}
---
+++ MFCC/Behavior/TintColorBehavior.ios.cs
@@ -0,0 +1,17 @@
+﻿using UIKit;
+
+namespace MFCC
+{
+    public partial class TintColorBehavior : PlatformBehavior<Image, UIImageView>
+    {
+        protected override void OnAttachedTo(Image bindable, UIImageView platformView)
+        {
+            ImageEx.ApplyColor(platformView, TintColor);
+        }
+
+        protected override void OnDetachedFrom(Image bindable, UIImageView platformView)
+        {
+            ImageEx.ClearColor(platformView);
+        }
+    }
+}
